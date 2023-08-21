cb7bb98c2523faba9a1019f7a27db86a255f8709
Caroline <caroline.boaventura08@gmail.com>
2023-08-21 17:31:23 -0300

    Criando as entidades de modelo, inicialmente como um modelo anêmico

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+namespace CleanArchMvc.Domain.Entities;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
