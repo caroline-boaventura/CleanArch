5491a5ba6f4459500131650df5baa6318c8bf98e
Caroline <caroline.boaventura08@gmail.com>
2023-08-28 18:17:27 -0300

    Transformando o modelo Category em um modelo rico

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,8 +1,36 @@
+using CleanArchMvc.Domain.Validation;
+
 namespace CleanArchMvc.Domain.Entities;
-public class Category
+public sealed class Category
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+
+    public Category(string name)
+    {
+        ValidateDomain(name);
+        
+    }
+
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value");
+        Id = id;
+        ValidateDomain(name);
+    }
+
+    public void Update(string name)
+    {
+        ValidateDomain(name);
+    }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+        DomainExceptionValidation.When(name.Length < 3, "Invalid name. Too short, minimum 3 charecters");
+
+        Name = name;
+    }
 
     public ICollection<Product> Products { get; set; }
 }
